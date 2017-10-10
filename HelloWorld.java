67380039d0b609aa45f4693700648318fe1fa53b
Meg Fryling <mfryling@siena.edu>
2017-10-10 15:47:09 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld
 {
     public static void main(String args[]) {
         System.out.println("Hello World"); 
+        System.out.println("Winter is coming! It just doesn't feel like it.");
     }
 }
